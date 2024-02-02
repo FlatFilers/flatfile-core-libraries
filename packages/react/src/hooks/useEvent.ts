5740b5d713851e360c4ddbceb01efd6d11bded50
Alex Rock <hi@lxrck.com>
2024-02-01 23:04:33 -0700

    feat: better off()

--- packages/react/src/hooks/useEvent.ts
+++ packages/react/src/hooks/useEvent.ts
@@ -5,18 +5,28 @@ import { useEffect } from 'react'
 export function useEvent(
   eventType: string,
   callback: (event: any) => void | Promise<void>,
+  filter?: Record<string, any>, // Added filter parameter
   dependencies: any[] = []
 ) {
   const { listener } = useFlatfile()
 
   useEffect(() => {
     if (listener) {
-      listener.on(eventType, callback)
+      // Check if a filter is provided and use it when registering the event
+      if (filter) {
+        listener.on(eventType, filter, callback)
+      } else {
+        listener.on(eventType, callback)
+      }
 
-      // Return a cleanup function that detaches the listener
+      // Return a cleanup function that detaches the listener with the same filter, if provided
       return () => {
-        listener.off(eventType, callback)
+        if (filter) {
+          listener.off(eventType, filter, callback)
+        } else {
+          listener.off(eventType, callback)
+        }
       }
     }
-  }, [...dependencies])
+  }, [listener, eventType, callback, filter, ...dependencies]) // Ensure filter is included in dependencies
 }
