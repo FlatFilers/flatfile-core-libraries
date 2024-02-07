dbd447e7f8d2dcca8485feadab02deacd232442f
Alex Rock <hi@lxrck.com>
2024-02-07 12:58:31 -0700

    feat: update index.ts location to match all other packages

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,5 @@
-import InitSpace from './src/components/InitSpace'
-import Space from './src/components/Space'
+import InitSpace from './components/InitSpace'
+import Space from './components/Space'
 
 import type {
   ISidebarConfig,
@@ -8,10 +8,10 @@ import type {
   IThemeConfig,
   IUserInfo,
 } from '@flatfile/embedded-utils'
-import { usePortal } from './src/hooks/usePortal'
-import { useSpace } from './src/hooks/useSpace'
-import { initializeFlatfile } from './src/hooks/useSpaceTrigger'
-import { makeTheme } from './src/utils/makeTheme'
+import { usePortal } from './hooks/usePortal'
+import { useSpace } from './hooks/useSpace'
+import { initializeFlatfile } from './hooks/useSpaceTrigger'
+import { makeTheme } from './utils/makeTheme'
 
 export { InitSpace, Space, initializeFlatfile, makeTheme, usePortal, useSpace }
 
