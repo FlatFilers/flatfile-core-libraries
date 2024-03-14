512be455a81325d3cbd3bb1ee15b5935c550165d
Alex Rock <hi@lxrck.com>
2024-03-13 20:20:45 -0600

    feat: test out all components bot creating a space and re-using a space

--- packages/react/src/utils/initializeSpace.tsx
+++ packages/react/src/utils/initializeSpace.tsx
@@ -4,9 +4,10 @@ import { authenticate } from './authenticate'
 import { getErrorMessage } from '@flatfile/embedded-utils'
 
 export const initializeSpace = async (
-  flatfileOptions: IReactSpaceProps
+  flatfileOptions: IReactSpaceProps & { sheet?: Flatfile.SheetConfig }
 ): Promise<Flatfile.SpaceResponse> => {
   let space
+  console.log('initializeSpace', { flatfileOptions })
   const {
     publishableKey,
     environmentId,
@@ -24,6 +25,7 @@ export const initializeSpace = async (
     labels,
     translationsPath,
     languageOverride,
+    sheet,
   } = flatfileOptions
 
   try {
@@ -36,7 +38,7 @@ export const initializeSpace = async (
     try {
       space = await limitedAccessApi.spaces.create({
         name,
-        autoConfigure: !workbook,
+        autoConfigure: !workbook && !sheet,
         ...spaceBody,
         labels: ['embedded', ...(labels || [])],
         ...(environmentId !== undefined && { environmentId }),
