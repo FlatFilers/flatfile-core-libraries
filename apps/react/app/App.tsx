2c12dec7a5d06c306f3f158539d3edeceb4bbf6a
Alex Rock <hi@lxrck.com>
2024-05-22 12:38:18 -0600

    feat: check if context is ready before triggerin openPortal()

--- apps/react/app/App.tsx
+++ apps/react/app/App.tsx
@@ -11,17 +11,24 @@ import {
   Document,
   Sheet,
 } from '@flatfile/react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
 import { recordHook } from '@flatfile/plugin-record-hook'
 
 const App = () => {
-  const { open, openPortal, closePortal, listener } = useFlatfile()
-
+  const { open, openPortal, closePortal } = useFlatfile()
   const [label, setLabel] = useState('Rock')
   const toggleOpen = () => {
     open ? closePortal({ reset: false }) : openPortal()
   }
+  // useEffect(() => {
+  //   if (!open) setTimeout(openPortal, 1000)
+  // }, [open])
+  
+  useEffect(() => {
+    openPortal()
+  }, [])
+
   useListener((listener) => {
     listener.on('**', (event) => {
       console.log('FFApp useListener Event => ', {
