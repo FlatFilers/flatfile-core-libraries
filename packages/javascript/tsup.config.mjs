e1ecad8c691a6be7506f17b0c832c8d2342c6bef
Carl Brugger <carl.brugger@flatfile.io>
2024-10-23 17:36:35 -0500

    feat: tsup & arethetypeswrong for JS SDK

---
+++ packages/javascript/tsup.config.mjs
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv'
+import { defineConfig } from 'tsup'
+
+dotenv.config()
+
+const PROD = process.env.NODE_ENV === 'production'
+if (!PROD) {
+  console.log('Not in production mode - skipping minification')
+}
+
+const nodeConfig = {
+  name: 'node',
+  platform: 'node',
+  minify: process.env.NODE_ENV === 'production',
+  entryPoints: ['index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  outDir: 'dist',
+  clean: true,
+  outExtension: ({ format }) => ({
+    js: format === 'cjs' ? '.cjs' : '.js',
+  }),
+}
+
+const browserConfig = {
+  name: 'browser',
+  platform: 'browser',
+  minify: process.env.NODE_ENV === 'production',
+  entryPoints: ['index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  outDir: 'dist',
+  clean: true,
+  outExtension: ({ format }) => ({
+    js: format === 'cjs' ? '.browser.cjs' : '.browser.js',
+  }),
+}
+
+export default defineConfig([nodeConfig, browserConfig])
