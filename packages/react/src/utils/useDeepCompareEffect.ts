98200caec06c756a2677d3b0c124a5d2480a141e
Carl Brugger <carl.brugger@flatfile.io>
2024-12-06 09:45:26 -0600

    Move/remove/replace lodash

--- packages/react/src/utils/useDeepCompareEffect.ts
+++ packages/react/src/utils/useDeepCompareEffect.ts
@@ -1,11 +1,40 @@
-import { isEqual } from 'lodash'
-import { useRef, useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export function useDeepCompareEffect(callback: () => void, dependencies: any) {
   const currentDependenciesRef = useRef()
 
+  const deepEqual = (obj1: any, obj2: any) => {
+    if (obj1 === obj2) {
+      return true
+    }
+
+    if (
+      typeof obj1 !== 'object' ||
+      obj1 === null ||
+      typeof obj2 !== 'object' ||
+      obj2 === null
+    ) {
+      return false
+    }
+
+    let keysObj1 = Object.keys(obj1)
+    let keysObj2 = Object.keys(obj2)
+
+    if (keysObj1.length !== keysObj2.length) {
+      return false
+    }
+
+    for (let key of keysObj1) {
+      if (!keysObj2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+        return false
+      }
+    }
+
+    return true
+  }
+
   useEffect(() => {
-    if (!isEqual(currentDependenciesRef.current, dependencies)) {
+    if (!deepEqual(currentDependenciesRef.current, dependencies)) {
       callback()
     }
 
